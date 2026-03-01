a6f87fa0a1297f075753360222d8b7a1d848152c
Varalakshmamma <hrvara321@gmail.com>
2026-03-01 12:15:43 +0530

    Day-22 Internship work

--- src/CreateEnquiry.jsx
+++ src/CreateEnquiry.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import {usestate} from 'react';
+import { useState } from "react";
 
 
 function CreateEnquiry() {
 
-    const [enquiry,setEnquiry]=usestate({userName:"", password:""})
+    const [enquiry,setEnquiry]=useState({userName:"", password:""})
+
+    function handleChange(e){
+        const {name,value}=e.target.value;
+        setEnquiry((oldval)=>({...oldval,[name]:value}))
+     
+    }
+
     return (
         <div>
             Create Enquiry
             <form>
-                <input type="text" name="" id="" />
-                <input type="password" name="" id="" />
+                <input type="text" name="userName" id="" placeholder='userName' onChange={handleChange} />
+                <input type="password" name="password" id="" placeholder='password' onChange={handleChange}/>
                 <button type='submit'>Submit btn</button>
             </form>
         </div>
