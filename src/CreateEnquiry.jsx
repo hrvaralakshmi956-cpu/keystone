1354ebb5183c73603d2635297fc505ebbf0f3f8e
Varalakshmamma <hrvara321@gmail.com>
2026-03-04 08:24:31 +0530

    Day-23 Internship work

--- src/CreateEnquiry.jsx
+++ src/CreateEnquiry.jsx
@@ -1,23 +1,26 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react';
+import { useState } from 'react';
 
 
 function CreateEnquiry() {
 
-    const [enquiry,setEnquiry]=useState({userName:"", password:""})
+    const [enquiry,setEnquiry]=useState({userName:"", password:"",Age:0,FatherName:""})
 
     function handleChange(e){
-        const {name,value}=e.target.value;
-        setEnquiry((oldval)=>({...oldval,[name]:value}))
+        //extraction
+        const {name,value}=e.target;
+        setEnquiry((oldval)=>({...oldval,[name]:value}));
      
     }
-
+    console.log("enquiry obj",enquiry)
     return (
         <div>
             Create Enquiry
             <form>
                 <input type="text" name="userName" id="" placeholder='userName' onChange={handleChange} />
                 <input type="password" name="password" id="" placeholder='password' onChange={handleChange}/>
+                <input type="number" name="Age" id=""  placeholder='Age' onChange={handleChange}/>
+                <input type="text" name="FatherName" id="" placeholder='FatherName' onChange={handleChange} />
                 <button type='submit'>Submit btn</button>
             </form>
         </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -14,6 +14,14 @@ function Home() {
     {
         alert("avg");
     }
+    const sampleProducts=[
+        {name:"Bangles",
+        price:100,
+        image:"https://i.etsystatic.com/24023014/r/il/425ffc/2500842045/il_fullxfull.2500842045_4rub.jpg"},
+        {name:"bow",price:50,
+        img:"https://tse4.mm.bing.net/th/id/OIP.BQyxW4aRGo3if6OgB6ZWcwHaHa?pid=Api&P=0&h=180"},
+
+    ];
 
     return (
         <div>
@@ -23,6 +31,17 @@ function Home() {
 
             <Section userName={"Vara"} phoneNo={"030-2203-2341"}/>
             <Welcome generateAvg={getAvg}/>
+            {/* Rendering list in react js */}
+
+            {
+              sampleProducts.map((ele)=>{
+                return <div className="productCard">
+                    <p>Product Name is : {ele.name}</p>
+                    <p> price :{ele.price}</p>
+                    <img src={ele.image} alt="" width={100} height={100} />
+                    </div>
+              }) 
+            }
         </div>
     );
 }
